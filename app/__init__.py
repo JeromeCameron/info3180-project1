c38560d9f6c2df042eb6133a946cdcf7a7b2d8e2
Jerome Cameron <jerome.cameron@mymona.uwi.edu>
2019-03-15 04:47:53 +0000

    deploy to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,8 @@ from flask import session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "@#$%my$ecretKey@#$%*"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ldeeolffifzwrh:f964dc6f33fdbaf21381\
+0db7cb63927883136874f3338c595560d062a9ba046b@ec2-54-221-201-212.compute-1.amazonaws.com:5432/d8h7mmdiiajgrm' #"postgresql://project1:project1@localhost/project1"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 UPLOAD_FOLDER = './app/static/uploads'
 
